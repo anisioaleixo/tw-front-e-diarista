940e1679d261c3bf122d9fe0e7a338ee037b17f6
anisioaleixo <anisioaleixo@yahoo.com.br>
2021-10-11 19:38:55 -0300

    organizando componente

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,18 @@
-import type { NextPage } from 'next'
-import PageTitle from 'ui/components/feedback/data-display/PageTitle/PageTitle'
-import SafeEnvironment from 'ui/components/feedback/SafeEnvironment/SafeEnvironment'
+import type { NextPage } from "next";
+import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
+import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
 const Home: NextPage = () => {
   return (
     <div>
       <SafeEnvironment />
       <PageTitle
-        title={'Conheça os profissionais'}
-        subtitle={'Preença seu endereço e veja todos os profissionais da sua localidade'} />
+        title={"Conheça os profissionais"}
+        subtitle={
+          "Preença seu endereço e veja todos os profissionais da sua localidade"
+        }
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
